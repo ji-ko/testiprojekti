b8b6b7ad9b2ccfc5db22d91523f2e2b1e3002477
Juhani K <juhani.kantola@iki.fi>
2024-03-28 12:42:40 +0200

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World MODIFIED!</h1>')
+  res.send('<h1>Totally new image!</h1>')
 })
 
 const PORT = 8080
